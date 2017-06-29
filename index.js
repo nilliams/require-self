bb60fcb8f1a7a25774ff92c5f26450eddf0fc4c4
Nick Williams <nickwilliams85@gmail.com>
2017-06-29 11:23:00 +0100

    Re-run prettier with semis enabled

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-var fs = require('fs')
-var path = require('path')
-var mkdirp = require('mkdirp')
+var fs = require('fs');
+var path = require('path');
+var mkdirp = require('mkdirp');
 
 module.exports = function createPseudoModule(cwd, name, entryPoint) {
   // Compute the location and content for the pseudo-module.
-  var modulePath = path.join(cwd, 'node_modules', name, entryPoint)
+  var modulePath = path.join(cwd, 'node_modules', name, entryPoint);
   var moduleText =
     "module.exports = require('" +
     path.join(cwd, entryPoint).replace(/\\/g, '\\\\') +
-    "');"
+    "');";
 
   // Create the pseudo-module, use `mkdirp` to handle nested dirs.
-  mkdirp.sync(path.dirname(modulePath))
-  fs.writeFileSync(modulePath, moduleText)
-}
+  mkdirp.sync(path.dirname(modulePath));
+  fs.writeFileSync(modulePath, moduleText);
+};
